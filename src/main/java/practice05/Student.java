4602a8246ab009b24ccfa8adaca1f02b1d2cccf9
chanho <howard.chan@oocl.com>
2020-11-25 15:01:49 +0800

    [javaoo][Howard Chan]finish practice05

--- src/main/java/practice05/Student.java
+++ src/main/java/practice05/Student.java
@@ -1,4 +1,21 @@
 package practice05;
 
-public class Student {
+import practice04.Person;
+
+public class Student extends Person {
+
+    private final int Class;
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.Class = klass;
+    }
+
+    public int getKlass() {
+        return Class;
+    }
+
+    public String introduce() {
+        return super.introduce() + " I am a Student. I am at Class " + getKlass() + ".";
+    }
 }
--- src/main/java/practice05/Teacher.java
+++ src/main/java/practice05/Teacher.java
@@ -1,4 +1,23 @@
 package practice05;
 
-public class Teacher {
+public class Teacher extends Person {
+    private final int klass;
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+        klass = 0;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public String introduce() {
+        return super.introduce() + " I am a Teacher. I teach " + (klass != 0 ? "Class " + klass + "." : "No Class.");
+    }
 }
