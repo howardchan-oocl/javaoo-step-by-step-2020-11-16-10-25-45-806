04eb76f59bf64f225534ec2c6d7fdecbb06c9075
chanho <howard.chan@oocl.com>
2020-11-25 17:19:43 +0800

    [javaoo][Howard Chan]finish practice09

--- src/main/java/practice09/Person.java
+++ src/main/java/practice09/Person.java
@@ -1,4 +1,49 @@
 package practice09;
 
+import java.util.Objects;
+
 public class Person {
+    private final String name;
+    private final int age;
+    private final int id;
+
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String introduce() {
+        return "My name is " + getName() + ". I am " + getAge() + " years old.";
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return id;
+    }
+
+//    @Override
+//    public boolean equals(Object o) {
+//        Person test = (Person) o;
+//        return test.id==id?true:false;
+//    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return age == person.age &&
+                id == person.id &&
+                Objects.equals(name, person.name);
+    }
 }
--- src/main/java/practice09/Teacher.java
+++ src/main/java/practice09/Teacher.java
@@ -1,4 +1,53 @@
 package practice09;
 
-public class Teacher {
+import java.util.Collection;
+import java.util.LinkedList;
+
+public class Teacher extends Person {
+
+    private final LinkedList<Klass> linkedList;
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+        this.linkedList = null;
+    }
+
+    public Teacher(int id, String name, int age, LinkedList<Klass> linkedList) {
+        super(id, name, age);
+        this.linkedList = linkedList;
+    }
+
+    public String introduce() {
+        String klassName = "";
+        if(linkedList!=null) {
+            for (int i = 0; i < linkedList.size(); i++) {
+                klassName += linkedList.get(i).getNumber();
+                if (i != linkedList.size() - 1)
+                    klassName += ", ";
+            }
+        }
+        return super.introduce() + " I am a Teacher. I teach " + (linkedList != null ? "Class " + klassName + "." : "No Class.");
+    }
+
+    public String introduceWith(Student student) {
+        boolean isTeach = false;
+        for (int i = 0; i < linkedList.size(); i++) {
+            if (linkedList.get(i)==student.getKlass())
+                isTeach = true;
+        }
+        return super.introduce() + " I am a Teacher. I " + (isTeach? "teach " + student.getName() + "." : "don't teach Jerry.");
+    }
+
+    public LinkedList<Klass> getClasses() {
+        return linkedList;
+    }
+
+    public boolean isTeaching(Student student) {
+        boolean isTeach = false;
+        for (int i = 0; i < linkedList.size(); i++) {
+            if (linkedList.get(i)==student.getKlass())
+                isTeach = true;
+        }
+        return isTeach;
+    }
 }
