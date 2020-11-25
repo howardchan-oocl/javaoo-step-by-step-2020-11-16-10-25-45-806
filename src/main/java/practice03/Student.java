98845dff904678b0f66826fa3b8c6410d2cfb2c6
chanho <howard.chan@oocl.com>
2020-11-25 14:49:23 +0800

    [javaoo][Howard Chan]finish practice03

--- src/main/java/practice03/Student.java
+++ src/main/java/practice03/Student.java
@@ -1,4 +1,19 @@
 package practice03;
 
-public class Student {
+public class Student extends Person {
+
+    private final int Klass;
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.Klass = klass;
+    }
+
+    public int getKlass() {
+        return Klass;
+    }
+
+    public String introduce() {
+        return "I am a Student. I am at Class " + getKlass() + ".";
+    }
 }
